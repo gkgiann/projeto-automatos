38c49a03ef198860195b4b8e9cbb6bb8ce56879d
gian <gian@pop-os.localdomain>
2024-03-30 15:03:29 -0300

    update afd and afn

--- afd/afd.py
+++ afd/afd.py
@@ -1,112 +1,81 @@
+from time import sleep
+
 # L = { w ∊ {a,b}* | w tem como prefixo aa }
 
-# Σ = {a, b}
-# Q = {q0, q1, q2}
+# alfabeto = ['a', 'b']
+# estados = ['q0', 'q1', 'q2']
 
-# δ = {
-#       δ(q0, a) = q1
-#       δ(q1, a) = q2
-#       δ(q2, a) = q2
-#       δ(q2, b) = q2
-#     }
+# f_transicao = {
+#     ('q0', 'a'): 'q1',
 
-# q0 = q0
-# F = {q2}
+#     ('q1', 'a'): 'q2',
 
-cadeiaAlfabeto = input('\nDigite o alfabeto: ')
+#     ('q2', 'a'): 'q2',
+#     ('q2', 'b'): 'q2',
+# }
 
-alfabeto = []
-for simbolo in cadeiaAlfabeto:
-    alfabeto.append(simbolo)
+# estado_inicial = 'q0'
+# estados_finais = ['q2']
+
+alfabeto = input('Alfabeto (separando as letras com espaço): ').split()
 
 quantEstados = int(input('Digite a quantidade de estados: '))
 
 estados = []
 for i in range(quantEstados):
-    estados.append('q' + str(i))
+    estados.append(f'q{i}')
 
-print('\nAlfabeto: ' + str(alfabeto))
-print('Estados: ' + str(estados))
+print(f'\nAlfabeto: {alfabeto}')
+print(f'Estados: {estados}')
 
-f_transicao = {
+f_transicao = {}
 
-}
-print('\nAdicionar transições:\n')
-addTransicoes = 1
-while addTransicoes == 1:
-    estPartida = input('Digite o estado de partida (qi): ')
+print('\nAdicionar transições:')
+loop = 1
+while loop == 1:
+    estadoPartida = input('\nDigite o estado de partida (qi): ')
     simboloChave = input('Digite o símbolo do alfabeto: ')
-    estChegada = input('Digite o estado de chegada (qi): ')
+    estadoChegada = input('Digite o estado de chegada (qi): ')
 
-    f_transicao[(estPartida, simboloChave)] = estChegada
+    f_transicao[(estadoPartida, simboloChave)] = estadoChegada
 
-    print()
-
-    parar = input('Finalizou? (s/n): ')
+    parar = input('\nFinalizou? (s/n): ')
     if parar == 's':
-        addTransicoes = 0
+        loop = 0
 
+
+estado_inicial = input('\nDigite o estado inicial do autômato (qi): ')
+estados_finais = input('Digite os estados finais (separa com espaço): ').split()
+
+loop = 1
+while loop == 1:
+    cadeia = input('\nCadeia para testar: ')
     print()
 
-estado_inicial = input('Digite o estado inicial do autômato (qi): ')
-estado_atual_sempre = estado_inicial
-
-estados_finais = []
-addEstadosFinais = 1
-while addEstadosFinais == 1:
-    estados_finais.append(input('Digite 1 estado final do autômato (qi): '))
-
-    fimEstadosFinais = input('Fim de estados finais? (s/n): ')
-    if fimEstadosFinais == 's':
-        addEstadosFinais = 0
-
-# ALFABETOS E ESTADOS JÁ DEFINIDOS
-"""
-alfabeto = ['a', 'b']
-estados = ['q0', 'q1', 'q2']
-
-f_transicao = {
-    ('q0', 'a'): 'q1',
-
-    ('q1', 'a'): 'q2',
-
-    ('q2', 'a'): 'q2',
-    ('q2', 'b'): 'q2',
-}
-
-estado_inicial = 'q0'
-estados_finais = ['q2']
-estado_atual = estado_inicial
-"""
-
-testCadeias = 1
-while testCadeias == 1:
-    cadeia = input('Cadeia para testar: ')
-    print()
-
-    estado_atual = estado_atual_sempre
+    estado_atual = estado_inicial
 
     try:
         for simbolo in cadeia:
             estado_anterior = estado_atual
             estado_atual = f_transicao[(estado_atual, simbolo)]
 
-            print('Atual: ' + estado_anterior + " - Símbolo: " + simbolo + ' --->' + ' Virou: ' + estado_atual)
+            sleep(1)
+            print(f'{estado_anterior} - {simbolo} ---> {estado_atual}')
 
     except KeyError:
-        print('!# ERRO: O símbolo ' + simbolo + ' não foi aceito no estado ' + estado_anterior + ' #!')
+        sleep(1)
+        print(f'\033[31mERRO: O autômato travou no estado {estado_anterior} ao tentar consumir o simbolo {simbolo}\033[0m')
         estado_atual = None
 
     print()
-
+    sleep(1)
     if estado_atual in estados_finais:
-        print('Cadeia aceita!\n')
+        print('\033[32mCadeia aceita!\033[0m')
     else:
-        print('Cadeia rejeitada!\n')
+        print('\033[31mCadeia rejeitada!\033[0m')
 
-    finalizar = input('Deseja finalizar? (s/n): ')
+    sleep(1)
+    finalizar = input('\nDeseja finalizar? (s/n): ')
     if finalizar == 's':
-        testCadeias = 0
-        print('Autômato AFD finalizado!')
-
-    print()
+        loop = 0
+        print('\n\033[1;34mAutômato AFD finalizado!\033[0m')
--- afn/afn.py
+++ afn/afn.py
@@ -1,22 +1,56 @@
+from time import sleep
+
 # L = { w | w possuí aa ou bb como subpalavra }
 
-alfabeto = ['a', 'b']
-estados = ['q0', 'q1', 'q2', 'q3']
+# alfabeto = ['a', 'b']
+# estados = ['q0', 'q1', 'q2', 'q3']
 
-f_transicao = {
-    ('q0', 'a'): ['q0', 'q1'],
-    ('q0', 'b'): ['q0', 'q2'],
+# f_transicao = {
+#     ('q0', 'a'): ['q0', 'q1'],
+#     ('q0', 'b'): ['q0', 'q2'],
 
-    ('q1', 'a'): ['q3'],
+#     ('q1', 'a'): ['q3'],
 
-    ('q2', 'b'): ['q3'],
+#     ('q2', 'b'): ['q3'],
     
-    ('q3', 'a'): ['q3'],
-    ('q3', 'b'): ['q3'],
-}
+#     ('q3', 'a'): ['q3'],
+#     ('q3', 'b'): ['q3'],
+# }
 
-estado_inicial = 'q0'
-estados_finais = ['q3']
+# estado_inicial = 'q0'
+# estados_finais = ['q3']
+
+alfabeto = input('Alfabeto (separando as letras com espaço): ').split()
+
+quantEstados = int(input('Digite a quantidade de estados: '))
+
+estados = []
+for i in range(quantEstados):
+    estados.append(f'q{i}')
+
+print(f'\nAlfabeto: {alfabeto}')
+print(f'Estados: {estados}')
+
+f_transicao = {}
+
+print('\nAdicionar transições:')
+loop = 1
+
+while loop == 1:
+    estadoPartida = input('\nEstado de partida: ')
+    simboloChave = input('Símbolo do alfabeto: ')
+    estadoChegada = input('Estados de chegada (separa com espaço): ').split()
+
+    f_transicao[(estadoPartida, simboloChave)] = estadoChegada
+
+    parar = input('\nFinalizou? (s/n): ')
+    if parar == 's':
+        loop = 0
+
+
+estado_inicial = input('\nEstado inicial do autômato: ')
+estados_finais = input('Estados finais (separa com espaço): ').split()
+
 
 def transicoes(estado_atual, simbolo):
     try:
@@ -31,22 +65,45 @@ def aceita_cadeia(cadeia):
     for simbolo in cadeia:
         estados_possiveis = []
 
+        print('=' * 70)
+        sleep(1)
+        print(f'Estados atuais: {estados_atuais}')
+        sleep(1)
+        print(f'Símbolo atual: {simbolo}')
+
+        sleep(1)
         for estado_atual in estados_atuais:
             estados_possiveis.extend(transicoes(estado_atual, simbolo))
+
+            print(f'Estados possíveis para seguir estando em {estado_atual}: {transicoes(estado_atual, simbolo)}')
             
+        sleep(1)
+        print(f'\nTodos os estados possíveis: {estados_possiveis}')
         estados_atuais = estados_possiveis
 
 
     for estado in estados_atuais:
         if estado in estados_finais:
+            print(f'\nParou no {estado} que é um estado final!')
             return True
 
     return False
 
 
-cadeia = input('Cadeia para testar: ')
+loop = 1
+while loop == 1:
+    cadeia = input('\nCadeia para testar: ')
+    print()
 
-if aceita_cadeia(cadeia):
-    print('Aceita')
-else:
-    print('Rejeita')
+    sleep(1)
+    if aceita_cadeia(cadeia):
+        print('\033[32mCadeia aceita!\033[0m')
+    else:
+        print('\033[31mCadeia rejeitada!\033[0m')
+        print('\n\033[31mO autômato não parou em um estado de aceitação!\033[0m')
+
+    sleep(1)
+    finalizar = input('\nDeseja finalizar? (s/n): ')
+    if finalizar == 's':
+        loop = 0
+        print('\n\033[1;34mAutômato AFD finalizado!\033[0m')
